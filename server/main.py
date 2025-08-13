51f9fd88efa6cd1412fa634caf0588da7b73cfb0
AjafaM <149159094+theo-picar1@users.noreply.github.com>
2025-08-13 13:21:28 +0100

    Initial commits

---
+++ server/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = ["http://localhost:3000"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
